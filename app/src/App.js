0fe04a8b7c8478a0318800767b2cbba5a60ca8dd
iyy0v <naitmihoub.ayoub@gmail.com>
2022-09-16 14:49:38 +0100

    page structure

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,27 @@
+import './style/index.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-        <p>Hello World</p>
+    <div className="App h-full bg-gradient-to-br from-slate-900 to-sky-900">
+      <NavBar />
+      <main className="flex pt-16">
+        <aside className="basis-1/4">
+          <div className="sticky top-16">
+            <p className="text-6xl">Hello  World</p>
+            <p className="text-6xl">Hello  World</p>
+          </div>
+        </aside>
+        <div className="basis-3/4">
+          <p className="text-9xl">Hello  World</p>
+          <p className="text-9xl">Hello  World</p>
+          <p className="text-9xl">Hello  World</p>
+          <p className="text-9xl">Hello  World</p>
+          <p className="text-9xl">Hello  World</p>
+          <p className="text-9xl">Hello  World</p>
+        </div>
+        
+      </main>
     </div>
   );
 }
---
+++ app/src/components/NavBar.js
@@ -0,0 +1,11 @@
+
+export default function NavBar() {
+
+    return (
+        <nav className="fixed flex flex-row flex-nowrap justify-between w-full h-16 backdrop-blur-md shadow-sm shadow-gray-800">
+            <div>LEFT SIDE</div>
+            <div>SEARCH AREA</div>
+            <div>RIGHT</div>
+        </nav>
+    );
+}
