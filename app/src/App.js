a02a2642ccb49360411d068aa3281ff03d724670
iyy0v <naitmihoub.ayoub@gmail.com>
2022-09-19 21:58:02 +0100

    get info (euw only)

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,18 @@
 import './style/index.css';
 import NavBar from './components/NavBar';
 import AccCard from './components/AccountCard';
+import { useState } from 'react';
 
 function App() {
+  const [accInfo, setAccInfo] = useState('');
+  const addAccount = (info) => {
+    setAccInfo(info);
+    console.log(info);
+  }
+
   return (
     <div className="App h-screen bg-gradient-to-b from-slate-900 to-sky-900  text-gray-100 overflow-hidden">
-      <NavBar />
+      <NavBar addAcc={addAccount}/>
       <main className=" h-[100vh] flex divide-x divide-sky-900 z-1">
         <aside className="basis-1/4 pt-16">
           <div className="sticky h-[92vh] overflow-y-scroll overscroll-contain scrollbar pt-4 flex flex-col items-center top-16 divide-y divide-sky-900">
--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -1,7 +1,29 @@
+import axios from 'axios';
 
-export default function NavBar() {
+export default function NavBar({addAcc}) {
+    
+    const API_KEY = "RGAPI-e2792f24-186c-4da9-a6cb-ef801611bc3c";
     let regions = [<option value="">EUW</option>,<option value="">EUNE</option>,<option value="">NA</option>,<option value="">KR</option>,<option value="">OCE</option>,<option value="">JP</option>,<option value="">BR</option>,<option value="">LAS</option>,<option value="">LAN</option>,<option value="">RU</option>,<option value="">TR</option>]
 
+    function handleGet(event) {
+        if(event.key === 'Enter') {
+            axios({
+          
+              // Endpoint to send files
+              url: "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + event.target.value + "?api_key=" + API_KEY,
+              method: "GET"
+            })
+          
+              // Handle the response from backend here
+              .then((res) => {
+                addAcc(res);
+            })
+          
+              // Catch errors if any
+              .catch((err) => { console.log(err)});
+        }
+    };
+    
     return (
         <nav className="fixed flex flex-row flex-nowrap justify-between items-center z-10 w-full h-16 pr-4 pl-4 backdrop-blur-md shadow-sm shadow-slate-800">
             <div>LEFT SIDE</div>
@@ -9,9 +31,9 @@ export default function NavBar() {
                 <select className="h-10 p-2 rounded-l outline-none bg-slate-200 text-gray-900 font-bold">
                     {regions}
                 </select>
-                <input type="text" placeholder="Summoner name here" className="h-10 p-2 rounded-r outline-none text-gray-900 font-medium"/>
+                <input type="text" placeholder="Summoner name here" onKeyPress={handleGet} className="h-10 p-2 rounded-r outline-none text-gray-900 font-medium"/>
             </div>
-            <div>RIGHT</div>
+            <div>RIGHT SIDE</div>
         </nav>
     );
 }
